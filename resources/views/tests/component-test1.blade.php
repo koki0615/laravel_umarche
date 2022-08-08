9601e4404aa8a84db6f657a84bf0365d07161d9c
koki0615 <onioni0615@gmail.com>
2022-08-09 03:08:15 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,5 +6,6 @@
 
     <x-tests.card title="タイトル1" content="本文" :message="$message"/>
     <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" ></x-tests.card>
 
 </x-tests.app>
