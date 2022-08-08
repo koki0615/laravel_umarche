aad1bf2032ae25548569b5a7370f9984fb8f011b
koki0615 <onioni0615@gmail.com>
2022-08-09 03:59:29 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- He who is contented is rich. - Laozi -->
+
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,6 +2,10 @@
     <x-slot name="header">
         ヘッダー2
     </x-slot>
-    
+
     コンポーネントテスト２
+    <x-test-class-base  classBaseMessage="メッセージです"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです。" defaultMessage="初期値から変更しています。"></x-test-class-base>
+
 </x-tests.app>
