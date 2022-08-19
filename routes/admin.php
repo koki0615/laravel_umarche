2e2674af6fabad06ad0083c6fe18d1d314f2ebcb
koki0615 <onioni0615@gmail.com>
2022-08-20 05:35:25 +0900

    middlewara, abort(404)

--- routes/admin.php
+++ routes/admin.php
@@ -30,9 +30,9 @@ Route::resource('owners', OwnersController::class)
 ->middleware('auth:admin')->except(['show']);
 
 Route::prefix('expired-owners')->
-middleware('auth:admin')->group(function(){
-Route::get('index',[OwnersController::class,'expiredOwnerIndex'])->name('expired-owners.index');
-Route::post('destroy/{owner}',[OwnersController::class, 'expiredOwnerDestroy'])->name('expired-owners.destroy');
+    middleware('auth:admin')->group(function(){
+        Route::get('index',[OwnersController::class,'expiredOwnerIndex'])->name('expired-owners.index');
+        Route::post('destroy/{owner}',[OwnersController::class, 'expiredOwnerDestroy'])->name('expired-owners.destroy');
 });
 
 Route::get('/dashboard', function () {
