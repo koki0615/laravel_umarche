2d5b78d2b5ca743a05d507579c461b8a5ca385f5
koki0615 <onioni0615@gmail.com>
2022-08-20 17:29:36 +0900

    image index

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Shop;
+use App\Models\Image;
 
 class Owner extends Authenticatable
 {
@@ -47,5 +48,9 @@ class Owner extends Authenticatable
     public function shop()
     {
         return $this->hasOne(Shop::class);
+    }
+
+    public function image() {
+        return $this->hasMany(Image::class);
     }
 }
