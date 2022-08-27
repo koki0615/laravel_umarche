83213a2c709cd7ba7b1caf772d777d46e950c78e
koki0615 <onioni0615@gmail.com>
2022-08-28 02:03:48 +0900

    constant

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -48,14 +48,14 @@
                             </div>
                             <div class="p-2 w-1/2 mx-auto">
                               <div class="relative flex justify-around">
-                                <div><input type="radio" name="type" value="1" class="mr-2" checked>追加</div>
-                                <div><input type="radio" name="type" value="2" class="mr-2" >削減</div>
+                                <div><input type="radio" name="type" value={{ \Constant::PRODUCT_LIST['add'] }} class="mr-2" checked>追加</div>
+                                <div><input type="radio" name="type" value={{ \Constant::PRODUCT_LIST['reduce'] }} class="mr-2" >削減</div>
                               </div>
                             </div>  
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                   <label for="quantity" class="leading-7 text-sm text-gray-600">数量 ※必須</label>
-                                  <input type="number" id="quantity" name="quantity" value="{{ $quantity }}"  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                  <input type="number" id="quantity" name="quantity" value=0  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                   <span class="text-sm">0~99の範囲で入力してください</span>
                                 </div>
                             </div>
