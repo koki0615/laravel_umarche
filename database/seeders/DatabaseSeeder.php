5d874c53db77cb4d5e613e4ac0dd0b6af165786c
koki0615 <onioni0615@gmail.com>
2022-08-29 01:27:14 +0900

    route, itemcontroller index

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             CategorySeeder::class,
             // ProductSeeder::class,
-            // StockSeeder::class
+            // StockSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
