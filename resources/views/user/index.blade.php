6d534d2e1a33df085db4ffd5a962803cf71bf28a
koki0615 <onioni0615@gmail.com>
2022-08-31 14:08:54 +0900

    表示件数

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -38,7 +38,24 @@
                             </select>
                         </div>
                         <div>
-                             表示件数
+                            <span class="text-sm">表示件数</span><br>
+                            <select id="pagination" name="pagination" class="mr-4">
+                                <option value="20"
+                                    @if(\Request::get('pagination') === '20')
+                                    selected
+                                    @endif>20件
+                                </option>
+                                <option value="50"
+                                    @if(\Request::get('pagination') === '50')
+                                    selected
+                                    @endif>50件
+                                </option>
+                                <option value="100"
+                                    @if(\Request::get('pagination') === '100')
+                                    selected
+                                    @endif>100件
+                                </option>
+                            </select>
                         </div>
                     </div>
                 </form>
@@ -67,12 +84,21 @@
                               </div>
                           @endforeach
                     </div>
+                    {{ $products->appends([
+                        'sort' => \Request::get('sort'),
+                        'pagination' => \Request::get('pagination')
+                    ])->links() }}
                 </div>
             </div>
         </div>
     </div>
 <script>
     const select = document.getElementById('sort')
+    select.addEventListener('change', function(){
+        this.form.submit()
+    })
+
+    const paginate = document.getElementById('pagination')
     select.addEventListener('change', function(){
         this.form.submit()
     })
